dfb5672cd19e7e343e10eb00c38d2ee21691012b
Richard Thio <richardthiorichard@gmail.com>
2024-11-12 18:37:46 +0000

    Add total price and selected by item class attributes

--- src/classes/Item.js
+++ src/classes/Item.js
@@ -1,11 +1,13 @@
 import { v4 as uuidv4 } from 'uuid'
 
 class Item {
-    constructor(name, unitPrice, quantity){
+    constructor(name, unitPrice, quantity, totalPrice){
         this.itemId = uuidv4();
         this.name = name;
         this.unitPrice = parseFloat(unitPrice);
         this.quantity = parseInt(quantity);
+        this.totalPrice = totalPrice;
+        this.selectedBy = [];
     }
 }
 
