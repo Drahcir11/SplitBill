a406551cbaecdf94378d53f4f7395533a32e24c0
Richard Thio <richardthiorichard@gmail.com>
2024-11-16 14:37:52 +0000

    Replace inline css with css page for friendslist page

--- src/FriendsList/FriendsList.js
+++ src/FriendsList/FriendsList.js
@@ -23,92 +23,46 @@ function FriendsList() {
     };
 
     return (
-        <>
-            <div className="friends-list">
-                <div
-                    style={{
-                        width: "85%",
-                        maxWidth: "420px",
-                    }}
-                >
-                    <div
-                        className="heading"
-                        style={{
-                            textAlign: "center",
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center",
-                        }}
-                    >
-                        <h1>FRIENDS</h1>
-                        <div style={{ width: "85%", textAlign: "center", textJustify: "center", marginTop: "4px", marginBottom: "24px" }}>
-                            <h5
-                                style={{
-                                    fontStyle: "sans-serif",
-                                    fontWeight: "700",
-                                    fontSize: "12px",
-                                    marginBlockStart: "8px",
-                                    marginBlockEnd: "8px",
-                                    color: "black",
-                                    textAlign: "center",
-                                    // color: "#5c5c5c",
-                                }}
-                            >
-                                Add your list of friend's names here.
-                            </h5>
-                        </div>
-                    </div>
-                    <form onSubmit={handleSubmit}>
-                        <div className="input-container">
-                            <input
-                                type="text"
-                                value={friendName}
-                                placeholder=" e.g. Karen"
-                                onChange={(e) => {
-                                    if (isValidInput(e.target.value)) {
-                                        setFriendName(e.target.value);
-                                    }
-                                }}
-                                // minLength={0}
-                                maxLength={50} //restricts User name input to 50 characters
-                                style={{
-                                    boxShadow: "4px 4px",
-                                    borderColor: "black",
-                                }}
-                            />
-                            <AddButton buttonName={"+"} type={"submit"} />
-                        </div>
-                    </form>
-                    <div className="name-list">
-                        <ol>
-                            {listOfFriends.map((friend, index) => {
-                                return <ObjectFriend key={index} friend={friend} />
-                            })}
-                        </ol>
-                    </div>
-                    <div
-                        className="button-container"
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "center",
-                        }}
-                    >
-                        <NextButton buttonName={"< NEXT PAGE >"} to={"/ReceiptCapture"} />
-                        <p
-                            style={{
-                                fontSize: "8px",
-                            }}
-                        >
-                            Manually add items
-                            <Link to="/item">
-                                <span style={{ textDecoration: "underline" }}> click here</span>
-                            </Link>
-                        </p>
-                    </div>
+        <div className="friends-list__container">
+                <div className="friends-list__text-content">
+                    <h1 className="friends-list__title">FRIENDS</h1>
+                    <h5 className="friends-list__heading">
+                        Add your list of friend's names here.
+                    </h5>
                 </div>
-            </div>
-        </>
+                <form onSubmit={handleSubmit}>
+                    <div className="friends-list__input-form">
+                        <input
+                            type="text"
+                            value={friendName}
+                            placeholder=" e.g. Karen"
+                            onChange={(e) => {
+                                if (isValidInput(e.target.value)) {
+                                    setFriendName(e.target.value);
+                                }
+                            }}
+                            maxLength={50} //restricts User name input to 50 characters
+                        />
+                        <AddButton buttonName={"+"} type={"submit"} />
+                    </div>
+                </form>
+                <div className="friends-list__friends-name">
+                    <ol>
+                        {listOfFriends.map((friend, index) => {
+                            return <ObjectFriend key={index} friend={friend} />
+                        })}
+                    </ol>
+                </div>
+                <div className="friends-list__button-container">
+                    <NextButton buttonName={"< NEXT PAGE >"} to={"/ReceiptCapture"} />
+                    <p>
+                        Manually add items
+                        <Link to="/item">
+                            <span style={{ textDecoration: "underline" }}> click here </span>
+                        </Link>
+                    </p>
+                </div>
+        </div>
     );
 }
 
