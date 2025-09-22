dfcc3b74dce71165413fba72e46476803f58e639
Richard Thio <richardthiorichard@gmail.com>
2025-09-22 22:31:32 +0100

    Add vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BillContextProvider } from "./context/BillContext.js";
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BillContextProvider>
             <App />
+            <Analytics />
         </BillContextProvider>
     </React.StrictMode>
 );
